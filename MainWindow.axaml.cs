759064d8d8796efa48aa1f600a40b7f11060d0c6
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-28 14:10:08 -0400

    add file dialog to button click method

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,32 @@
-using Avalonia.Controls;
-
 namespace CanvasCertificateGenerator;
+
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (StorageProvider is not null)
+        {
+            var options = new FolderPickerOpenOptions
+            {
+                Title = "Choose Certificate Folder Destination",
+            };
+
+            var result = await StorageProvider.OpenFolderPickerAsync(options);
+
+            if (result is not null)
+            {
+
+            }
+        }
     }
 }
