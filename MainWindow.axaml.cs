90d609832e7ef4580fbdf07bc0475edcfc29b38c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-04 18:04:38 -0400

    add text overlays for course name, participant name, date, and "This Certificate..." sentence

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -46,22 +46,31 @@ public partial class MainWindow : Window
             string participant = participantName.Text ?? string.Empty;
             string course = courseName.Text ?? string.Empty;
             DateTime date = completionDate.SelectedDate?.DateTime ?? DateTime.Today;
-            var dateStr = date.ToShortDateString();
-            string filename = $"{participant.Replace(" ", "_").ToLower()}{course.Replace(" ", "_").ToLower()}_certificate.pdf";
+            string filename = $"{participant.Replace(" ", "_").ToLower()}_{course.Replace(" ", "_").ToLower()}_certificate.pdf";
+            string participantRole = "participant";
 
             // set up PdfSharp document and page
             var document = new PdfDocument();
             var page = document.AddPage();
             page.Width = XUnit.FromPoint(2000);
             page.Height = XUnit.FromPoint(1545);
-
             var gfx = XGraphics.FromPdfPage(page);
 
-            // loads background image
             var background = XImage.FromFile("./Template.png");
             gfx.DrawImage(background, 0, 0, page.Width, page.Height);
 
+            var courseFont = new XFont("Geologica", 52, XFontStyle.Bold);
+            var nameFont = new XFont("Geologica", 65, XFontStyle.Bold);
+            var smallFont = new XFont("Roboto", 26, XFontStyle.Regular);
+            var dateFont = new XFont("Roboto", 24, XFontStyle.Regular);
+            var whiteBrush = XBrushes.White;
+            var yellowBrush = new XSolidBrush(XColor.FromArgb(255, 215, 0));
 
+            gfx.DrawString(participant, nameFont, whiteBrush, new XPoint(1220, 980), XStringFormats.Center);
+            gfx.DrawString(course.ToUpper(), courseFont, yellowBrush, new XPoint(1210, 485), XStringFormats.Center);
+            gfx.DrawString(date.ToShortDateString(), dateFont, whiteBrush, new XPoint(1495, 1408), XStringFormats.Center);
+            gfx.DrawString($"This Certificate is presented to {participant} for their outstanding", smallFont, whiteBrush, new XPoint(1220, 1130), XStringFormats.Center);
+            gfx.DrawString($"completion of the {course} course as a {participantRole}.", smallFont, whiteBrush, new XPoint(1220, 1170), XStringFormats.Center);
 
             document.Save(Path.Combine(path, filename));
         }
